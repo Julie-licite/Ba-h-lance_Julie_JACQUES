9d7a51e23e60827c66ce746f784e850eed8f3b23
Julie <julie.jacques.cours@gmail.com>
2020-11-06 18:18:07 +0100

    I forgot to create my git project.
    Rotation ok mais pas modifiable

---
+++ Assets/Inputs/Player.cs
@@ -0,0 +1,154 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/Player.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Player : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Player()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Player"",
+    ""maps"": [
+        {
+            ""name"": ""Character"",
+            ""id"": ""b2afd147-0f51-42ef-a495-39cccb25549f"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""af39c57a-7728-4624-896c-d4e9c8f087f5"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""Mouse"",
+                    ""id"": ""eba94603-6e3c-46d1-87c3-01381484be15"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""702c7ae1-ee6a-4ce4-8f6d-6afffac5a137"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""0787b1a2-03ad-43e0-bc4e-128204bbbf05"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Character
+        m_Character = asset.FindActionMap("Character", throwIfNotFound: true);
+        m_Character_Move = m_Character.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Character
+    private readonly InputActionMap m_Character;
+    private ICharacterActions m_CharacterActionsCallbackInterface;
+    private readonly InputAction m_Character_Move;
+    public struct CharacterActions
+    {
+        private @Player m_Wrapper;
+        public CharacterActions(@Player wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Character_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Character; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(CharacterActions set) { return set.Get(); }
+        public void SetCallbacks(ICharacterActions instance)
+        {
+            if (m_Wrapper.m_CharacterActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_CharacterActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public CharacterActions @Character => new CharacterActions(this);
+    public interface ICharacterActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    [SerializeField] private float pushStrengh;
+    [SerializeField] private float maxpush;
+    [SerializeField] private float maDirection;
+
+    private Player inputs;
+    //fonctionne
+    private Vector2 direction;
+    //test
+    //Vector3 myVector;
+   
+
+    private Rigidbody myRigidbody;
+
+    private void OnEnable()
+    {
+        //recup la liste des inputs
+        inputs = new Player();
+        inputs.Enable();
+        //recuperer l'action "quand le joueur appui sur les boutons d'actions move"
+        inputs.Character.Move.performed += OnMovePerformed;
+        //recuperer l'action "quand le joueur n'appui pas sur les boutons d'actions move"
+        inputs.Character.Move.canceled += OnMoveCanceled;
+        
+
+        //recup mon rigidbody et lui donne le nom d'une variable + facile a utiliser
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    //quand on utilise l'action move : 
+    private void OnMovePerformed(InputAction.CallbackContext obj)
+    {
+        //fonctionne
+        direction = obj.ReadValue<Vector2>();
+        //test
+        //myVector = obj.ReadValue<Vector2>();
+
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext obj)
+    {
+        //fonctione
+        direction = Vector2.zero;
+        //test
+        //myVector = Vector2.zero;
+    }
+
+    void FixedUpdate()
+    {
+        //celui qui marche
+        direction.y = 0;
+        //celui que je test
+        //myVector = new Vector3(0.0f, 0.0f, 0.0f);
+
+        //je limite ma force de poussée max
+        if (myRigidbody.velocity.sqrMagnitude < maxpush)
+        {
+            //celle qui fonctionne mais je choisi pas la direction.
+            myRigidbody.AddTorque(direction * pushStrengh  );
+            //celle qui devrait marcher mais t'as pas compris un truc
+            //myRigidbody.AddTorque(myVector* pushStrengh);
+            //la version addforceatposition de flore
+
+        }
+    }
+}
